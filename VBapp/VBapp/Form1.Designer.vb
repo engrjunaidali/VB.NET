a0895c60ce468380f2eaf376c2403c5d6e08062a
engrjunaidali <80405587+engrjunaidali@users.noreply.github.com>
2021-03-29 05:20:22 -0700

    Add files via upload

---
+++ VBapp/VBapp/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Register
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(125, 92)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(200, 89)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(284, 23)
+        Me.txtUsername.TabIndex = 1
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(200, 129)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(284, 23)
+        Me.txtPassword.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(125, 132)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Password"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(241, 183)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(141, 41)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Register"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Register
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Register"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ VBapp/VBapp/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Register
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer
+        If txtUsername.Text <> "" Then
+            a = a + 1
+        End If
+
+        If txtPassword.Text <> "" Then
+            a = a + 1
+        End If
+
+        If a = 2 Then
+            My.Settings.Username = Me.txtUsername.Text
+            My.Settings.Password = Me.txtPassword.Text
+            My.Settings.Save()
+
+            MessageBox.Show("Username and Password has been saved successfuly.")
+        Else
+            MessageBox.Show("Username or Password is not entered!")
+        End If
+
+
+    End Sub
+End Class
